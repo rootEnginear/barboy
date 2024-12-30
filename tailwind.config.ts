98c7810b7aaa08aaf01d49dc8f193f20a9566516
rootEnginear <rootenginear@users.noreply.github.com>
2024-12-30 09:53:47 +0700

    chore: tw configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,44 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		fontFamily: {
+			sans: [
+				'Atkinson Hyperlegible',
+				'ui-sans-serif',
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'Segoe UI',
+				'Roboto',
+				'Helvetica Neue',
+				'Arial',
+				'Noto Sans',
+				'Apple Color Emoji',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'Noto Color Emoji',
+				'sans-serif'
+			],
+			serif: [
+				'Atkinson Hyperlegible',
+				'ui-serif',
+				'Georgia',
+				'Cambria',
+				'Times New Roman',
+				'Times',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'Apple Color Emoji',
+				'Noto Color Emoji',
+				'sans-serif'
+			]
+		},
+		lineHeight: {
+			DEFAULT: '1.5',
+			1: '1',
+			1.5: '1.5',
+			1.6: '1.6'
+		},
 		extend: {}
 	},
 
